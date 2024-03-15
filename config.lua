40600695ed1c54aafe305999f92bbe426a57b548
DignityDC <98825734+DignityDC@users.noreply.github.com>
2024-03-15 13:41:26 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ Config.Vehicles = {
     'submersible',
     'submersible2',
 }
--- Props Can Be Found @ https://forge.plebmasters.de/objects/
+
 Config.Mines = {
     { name = 'h4_prop_h4_mine_01a', hash = 2509100520 },
     { name = 'xm_prop_x17_mine_01a', hash = 1039126093 },
@@ -16,3 +16,9 @@ Config.Mines = {
 }
 
 Config.DetectionRadius = 10.0 
+
+-- Discord Logs
+
+Config.Logs = true
+
+Config.Webhook = 'WEBHOOK_URL'
