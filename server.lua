0300f2d26fe96a232432c78058589ec5c3f09535
DignityDC <98825734+DignityDC@users.noreply.github.com>
2024-03-15 13:42:29 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,3 +2,22 @@ RegisterServerEvent('detectMine')
 AddEventHandler('detectMine', function(mineCoords, vehicleNetId)
     TriggerClientEvent('explodeMine', -1, mineCoords, vehicleNetId)
 end)
+
+
+RegisterServerEvent('logMineExplosion')
+AddEventHandler('logMineExplosion', function(playerServerId, vehicleName, mineHash, mineCoords)
+    if Config.Logs then
+        local playerName = GetPlayerName(playerServerId) or "Unknown Player"
+        local discordInfo = {
+            ["color"] = 15158332,
+            ["type"] = "rich",
+            ["title"] = "Mine Explosion Detected",
+            ["description"] = string.format("Player `%s` (%s) triggered mine `%s` with vehicle `%s` at coordinates [X: %.2f, Y: %.2f, Z: %.2f].", playerName, playerServerId, mineHash, vehicleName, mineCoords.x, mineCoords.y, mineCoords.z),
+            ["footer"] = {
+                ["text"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
+            }
+        }
+
+        PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({embeds = {discordInfo}}), {['Content-Type'] = 'application/json'})
+    end
+end)
